f319dd4f4c4e4bd0231e7ce523ccb3a809fa6dc5
Bruno Weber <contato@brunoweber.com.br>
2022-02-19 10:06:36 -0300

    feat(modules): creates the client creation module; creates the prismaClient.ts file
    closes #6

---
+++ src/modules/clients/useCases/createClient/CreateClientUseCase.ts
@@ -0,0 +1,40 @@
+import { prisma } from "../../../../database/prismaClient";
+
+import { hash } from "bcrypt"
+
+
+
+interface ICreateClient {
+  username: string;
+  password: string
+}
+
+export class CreateClientUseCase {
+  async execute({ username, password }: ICreateClient) {
+    // Validar se o usuário existe
+    const clientExist = await prisma.clients.findFirst({
+      where: {
+        username: {
+          mode: "insensitive"
+        }
+      }
+    })
+
+    if (clientExist) {
+      throw new Error("Client already exists!")
+    }
+
+    // Criptografar a senha
+    const hashPassword = await hash(password, 10)
+
+    // Salvar o client
+    const client = await prisma.clients.create({
+      data: {
+        username,
+        password: hashPassword
+      }
+    })
+
+    return client
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express, { request, response } from "express"
+import { routes } from "./routes"
 
 const app = express()
+
+app.use(express.json())
+app.use(routes)
 
 app.get("/", (request, response) => {
   return response.json({
