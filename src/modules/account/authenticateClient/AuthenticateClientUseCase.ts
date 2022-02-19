f9b12949616e1afcbac0031bfabf8ec9ff3e332e
Bruno Weber <contato@brunoweber.com.br>
2022-02-19 10:47:53 -0300

    feat(modules): creates the account module; creates client authentication
    closes #7

---
+++ src/modules/account/authenticateClient/AuthenticateClientUseCase.ts
@@ -0,0 +1,36 @@
+import { prisma } from "../../../database/prismaClient";
+import { compare } from "bcrypt"
+import { sign } from "jsonwebtoken"
+
+interface IAuthenticateClient {
+  username: string;
+  password: string
+}
+
+export class AuthenticateClientUseCase {
+  async execute({ username, password }: IAuthenticateClient) {
+    const client = await prisma.clients.findFirst({
+      where: {
+        username
+      }
+    })
+
+    if (!client) {
+      throw new Error("Username or password invalid!")
+    }
+
+    const passwordMatch = await compare(password, client.password)
+
+    if (!passwordMatch) {
+      throw new Error("Username or password invalid!")
+    }
+
+    const token = sign({ username }, "da810830ccf26cc3fcfd19370bdb9c4b", {
+      subject: client.id,
+      expiresIn: "1d"
+    })
+
+    return token
+
+  }
+}
