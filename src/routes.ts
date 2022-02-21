d01e501674eb1a6c5e73b7ff0c6886a8aec46b03
Bruno Weber <contato@brunoweber.com.br>
2022-02-21 10:24:24 -0300

    feat(modules): creates find all available deliveries without end date
    closes #10

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { AuthenticateClientController } from "./modules/account/authenticateClie
 import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController"
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController"
 import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController"
+import { FindAllAvailableController } from "./modules/deliveries/useCases/findAllAvailable/FindAllAvailableController"
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController"
 
 const routes = Router()
@@ -13,6 +14,7 @@ const authenticateClientController = new AuthenticateClientController()
 const createDeliverymanController = new CreateDeliverymanController()
 const authenticateDeliverymanController = new AuthenticateDeliverymanController()
 const deliveryController = new CreateDeliveryController()
+const findAllDeliveries = new FindAllAvailableController()
 
 routes.post("/client/authenticate", authenticateClientController.handle)
 routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handle)
@@ -21,5 +23,7 @@ routes.post("/delivery", ensureAuthenticatedClient, deliveryController.handle)
 
 routes.post("/client/", createClientController.handle)
 routes.post("/deliveryman", createDeliverymanController.handle)
+
+routes.get("/delivery/available", findAllDeliveries.handle)
 
 export { routes };
