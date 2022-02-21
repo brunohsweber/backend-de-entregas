3023be22a907fcbc740d2c0c7fe58048a1bd05a2
Bruno Weber <contato@brunoweber.com.br>
2022-02-21 09:28:41 -0300

    feat(modules): creates deliveries module; fix end_at in deliveries table
    closes #9

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express"
 import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController"
 import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController"
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController"
+import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController"
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController"
 
 const routes = Router()
@@ -10,9 +11,12 @@ const createClientController = new CreateClientController()
 const authenticateClientController = new AuthenticateClientController()
 const createDeliverymanController = new CreateDeliverymanController()
 const authenticateDeliverymanController = new AuthenticateDeliverymanController()
+const deliveryController = new CreateDeliveryController()
 
 routes.post("/client/authenticate", authenticateClientController.handle)
 routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handle)
+
+routes.post("/delivery", deliveryController.handle)
 
 routes.post("/client/", createClientController.handle)
 routes.post("/deliveryman", createDeliverymanController.handle)
