6a5946f4414b0190dfa52851a7c712ca40266ddf
Bruno Weber <contato@brunoweber.com.br>
2022-02-21 23:19:50 -0300

    feat(modules): create use case to update end date
    closes #19

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,7 @@ import { FindAllDeliveriesClientController } from "./modules/clients/useCases/de
 import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController"
 import { FindAllAvailableController } from "./modules/deliveries/useCases/findAllAvailable/FindAllAvailableController"
 import { UpdateDeliverymanController } from "./modules/deliveries/useCases/updateDeliveryman/UpdateDeliverymanController"
+import { UpdateEndDateController } from "./modules/deliveries/useCases/updateEndDate/UpdateEndDateController"
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController"
 import { FindAllDeliveriesDeliverymanController } from "./modules/deliveryman/useCases/findAllDeliveries/FindAllDeliveriesDeliverymanController"
 
@@ -22,6 +23,7 @@ const findAllDeliveriesController = new FindAllAvailableController()
 const updateDeliverymanController = new UpdateDeliverymanController()
 const findAllDeliveriesClientController = new FindAllDeliveriesClientController()
 const findAllDeliveriesDeliverymanController = new FindAllDeliveriesDeliverymanController()
+const updateEndDateController = new UpdateEndDateController()
 
 routes.post("/client/authenticate", authenticateClientController.handle)
 routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handle)
@@ -37,5 +39,7 @@ routes.put("/delivery/updateDeliveryman/:id", ensureAuthenticatedDeliveryman, up
 
 routes.get("/client/deliveries", ensureAuthenticatedClient, findAllDeliveriesClientController.handle)
 routes.get("/deliveryman/deliveries", ensureAuthenticatedDeliveryman, findAllDeliveriesDeliverymanController.handle)
+
+routes.put("/delivery/updateEndDate/:id", ensureAuthenticatedDeliveryman, updateEndDateController.handle)
 
 export { routes };
